4558ded5416613682332b7640413feb21104e0d2
archaion <80986857+archaion@users.noreply.github.com>
2023-02-09 15:02:17 -0600

    Update 2/9/23

--- sidebar-shop.php
+++ sidebar-shop.php
@@ -2,30 +2,31 @@
 /**
  * sidebar-shop.php
  * Shop sidebar - filters
+ * CHANGE PLACEHOLDER ATTRIBUTES TO PRODUCT ATTRIBUTE NAMES
  */
 if(! is_shop() && !is_archive()){
-    return;
+   return;
 }
 ?>
 <aside id="secondary" class="widget-area">
-    <div class="widget-area-inside">
-        <div class="entry-content <?php entry_content_class(); ?>">
-            <?php the_widget( 'WC_Widget_Product_Categories', array('count' => 1, 'hide_empty' => 1) ); ?>
-        </div>
-    </div>
-    <div class="widget-area-inside">
-        <div class="entry-content <?php entry_content_class(); ?>">
-        <h2 class="widgettitle"><?php echo esc_html( 'Filters') ?></h2>
-        <hr>
-        <?php the_widget( 'WC_Widget_Price_Filter', array() ); ?>
-        <div class="widget--size widget">
-            <?php the_widget( 'WC_Widget_Layered_Nav', array('title' => esc_html( 'Filter by size' ),'attribute'=>'size','display_type'=>'list', 'query_type' => 'or') ); ?>
-        </div>
-        <?php the_widget( 'WC_Widget_Layered_Nav', array('title' => esc_html( 'Filter by color' ), 'attribute'=>'color','display_type'=>'dropdown', 'query_type' => 'and') ); ?>
-        <?php the_widget( 'WC_Widget_Layered_Nav', array('title' => esc_html( 'Filter by size' ), 'attribute'=>'size','display_type'=>'dropdown', 'query_type' => 'or') ); ?>
-        <?php the_widget( 'WC_Widget_Rating_Filter', array() ); ?>
-        <?php the_widget( 'WC_Widget_Product_Tag_Cloud', array() ); ?>
-        </div>
-    </div> <!-- / widget-area -->
+   <div class="widget-area-inside">
+       <div class="entry-content <?php entry_content_class(); ?>">
+           <?php the_widget( 'WC_Widget_Product_Categories', array('title' => esc_html('Categories'),'count' => 1, 'hide_empty' => 1) ); ?>
+       </div>
+   </div>
+   <div class="widget-area-inside">
+       <div class="entry-content <?php entry_content_class(); ?>">
+       <hr>
+       <?php the_widget( 'WC_Widget_Price_Filter', array('title' => esc_html('Price')) ); ?>
+       <div class="widget--size widget">
+           <?php the_widget( 'WC_Widget_Layered_Nav', array('title' => esc_html( 'Attribute I' ), 'attribute'=>'goodness','display_type'=>'list', 'query_type' => 'or') ); ?>
+       </div>
+       <?php the_widget('WC_Widget_Layered_Nav', array('title' => esc_html( 'Attribute II' ), 'attribute'=>'kangaroos','display_type'=>'dropdown', 'query_type' => 'and') ); ?>
+       <?php the_widget( 'WC_Widget_Layered_Nav', array( 'title' => esc_html( 'Attribute III' ), 'attribute'=>'title','display_type'=>'dropdown', 'query_type' => 'or') ); ?>
+       <?php the_widget( 'WC_Widget_Rating_Filter', array() ); ?>
+       <hr>
+       <?php the_widget( 'WC_Widget_Product_Tag_Cloud', array('title' => esc_html( 'Tags' )) ); ?>
+       </div>
+   </div> <!-- / widget-area -->
 </aside><!-- #secondary -->
 <div class="clearfix"></div>
