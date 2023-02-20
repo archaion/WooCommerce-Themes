bd8dc832bd551a99dca846e473cf34530d9dcc63
archaion <80986857+archaion@users.noreply.github.com>
2023-02-20 15:30:51 -0600

    Update 2/20/23

--- search.php
+++ search.php
@@ -29,12 +29,18 @@ get_header();
       while (have_posts()) :
          the_post();
 
+			/**
+			 * Hook: woocommerce_shop_loop.
+			 */
+			do_action( 'woocommerce_shop_loop' );
+
+			wc_get_template_part( 'content', 'product' );
          /**
           * Run the loop for the search to output the results.
           * If you want to overload this in a child theme then include a file
           * called content-search.php and that will be used instead.
           */
-         get_template_part('template-parts/content', 'search');
+         //get_template_part('template-parts/content', 'search');
 
       endwhile;
 
