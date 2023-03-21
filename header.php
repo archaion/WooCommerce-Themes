45863b1bbfd57c4869489c542ab76ee00a3134b6
archaion <80986857+archaion@users.noreply.github.com>
2023-03-21 14:51:25 -0500

    Update3/21/23

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,52 +13,59 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+   <meta charset="<?php bloginfo('charset'); ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="profile" href="https://gmpg.org/xfn/11">
+
+   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'junkshop' ); ?></a>
-   <header id='banner'>
-      <div id='banner1'></div>
-      <div id='banner2'>IMPORT & DESIGN</div>
-   </header>
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$junkshop_description = get_bloginfo( 'description', 'display' );
-			if ( $junkshop_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $junkshop_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+   <?php wp_body_open(); ?>
+   <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'junkshop'); ?></a>
+      <header id='banner'>
+         <div id='banner1'></div>
+         <!--<div id='banner2'>IMPORT & DESIGN</div>-->
+      </header>
+      <header id="vidwrap">
+         <video id="vid" autoplay="true" muted="true" loop="true">
+            <source src="https://vampyre.tk/wp-content/themes/aaa/Slideshow.mp4" type="video/mp4">
+         </video>
+      </header>
+      <header id="masthead" class="site-header">
+         <div class="site-branding">
+            <?php
+            the_custom_logo();
+            if (is_front_page() && is_home()) :
+            ?>
+               <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php
+            else :
+            ?>
+               <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+            <?php
+            endif;
+            $junkshop_description = get_bloginfo('description', 'display');
+            if ($junkshop_description || is_customize_preview()) :
+            ?>
+               <p class="site-description"><?php echo $junkshop_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+                                             ?></p>
+            <?php endif; ?>
+         </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'junkshop' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			get_search_form();
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+         <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Menu', 'junkshop'); ?></button>
+            <?php
+            wp_nav_menu(
+               array(
+                  'theme_location' => 'menu-1',
+                  'menu_id'        => 'primary-menu',
+               )
+            );
+            get_search_form();
+            ?>
+         </nav><!-- #site-navigation -->
+      </header><!-- #masthead -->
